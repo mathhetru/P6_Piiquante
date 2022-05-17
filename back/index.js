a6118dbb10f08846bacc9bdcaf724bde47aa9f43
Mathilde Hetru <76809829+mathhetru@users.noreply.github.com>
2022-05-17 12:21:04 +0200

    etape 1

---
+++ back/index.js
@@ -0,0 +1,16 @@
+const http = require("http");
+const express = require("express");
+const bodyparser = require("body-parser");
+const jsonwebtoken = require("jsonwebtoken");
+
+const host = 'localhost';
+const port = 3000;
+const requestListener = function (req, res) {
+    res.writeHead(200);
+    res.end("My first server!");
+};
+const server = http.createServer(requestListener);
+server.listen(port, host, () => {
+    console.log(`Server is running on http://${host}:${port}`);
+});
+
